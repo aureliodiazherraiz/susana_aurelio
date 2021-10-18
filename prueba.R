e8a1c67ef432932cc1b95830f15826f351e942f4
Susana <s.silvestre@creaf.uab.cat>
2021-10-18 11:37:25 +0200

    cambio read csv a table

--- prueba.R
+++ prueba.R
@@ -1,3 +1,5 @@
 prueba <- "taller de git Plasencia"
 library(readr)
-dt<-read_csv("~/digital.agri/CSV/ datos_brutos_medias_250121.csv")
+dt<-read.table("~/digital.agri/CSV/ datos_brutos_medias_250121.csv")
+"hola soy Susana"
+
